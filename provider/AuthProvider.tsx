e7d582f141449ee7b4577086f660088dff5849a8
Uygar Eren <komifb90@hotmail.com>
2024-04-02 18:15:04 +0300

    AuthProvider was updated

--- provider/AuthProvider.tsx
+++ provider/AuthProvider.tsx
@@ -1,21 +1,41 @@
-import { View, Text } from "react-native";
-import { useEffect } from "react";
-import useI18n from "../hooks/useI18n";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView, Text, View } from "react-native";
 import { useSelector } from "react-redux";
+import { ButtonComp } from "../components/Button";
+import useI18n from "../hooks/useI18n";
 import { RootStateType } from "../store/store";
-import { WHITE } from "../utils/utils";
+import { PINK, WHITE } from "../utils/utils";
 
 
 export default function AuthProvider({children}: any){
+
+    const navigation = useNavigation<any>();
 
     const {t} = useI18n("AuthProvider");
     const userId = useSelector<RootStateType, any>((state:any) => state.user.userInfo.userId);
 
     if(userId == undefined){
         return(
-            <View style={{flex:1, backgroundColor: WHITE, justifyContent:"center", alignItems:"center"}}>
-                <Text>Oturum Açmanız Gerekmektedi</Text>
-            </View>
+            <SafeAreaView style={{flex:1, backgroundColor: WHITE, alignItems:"center", justifyContent:"center"}}>
+                
+                <View style={{width:"100%", alignItems:"center"}}>
+                    <Text style={{fontWeight:"600", fontSize:14}}>Oturum Açmanız Gerekmektedir!</Text>
+                    <ButtonComp title="Giriş Yap!" onPress={() => navigation.navigate("Profile")} styleContainer={{
+                        alignSelf: "center",
+                        alignItems: "center",
+                        marginTop: 20,
+                        backgroundColor: PINK,
+                        borderRadius: 25,
+                        paddingVertical: 12,
+                        width:"50%"
+                    }}
+                    styleText={{
+                        fontSize: 16,
+                        fontWeight: "500",
+                        color: WHITE
+                    }}/>
+                </View>
+            </SafeAreaView>
         )
     }
 
