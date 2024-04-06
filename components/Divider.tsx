7348a637d777c0e10a16e78e041cc744b44fdc43
Uygar Eren <komifb90@hotmail.com>
2024-04-06 22:22:31 +0300

    divider updated

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -1,14 +1,15 @@
-import { View } from "react-native"
+import { View, ViewStyle } from "react-native"
 import { Double } from "react-native/Libraries/Types/CodegenTypes"
 import { LIGHT_GRAY } from "../utils/utils"
 
 type DividerType = {
     height: number | Double,
-    width: any
+    width: any,
+    style?: ViewStyle
 }
 
-export const Divider: React.FC<DividerType> = ({height, width}) => {
+export const Divider: React.FC<DividerType> = ({height, width, style}) => {
     return(
-        <View style={{height:height, backgroundColor:LIGHT_GRAY, width: width, alignSelf: "center", marginTop:10}}/>
+        <View style={{height:height, backgroundColor:LIGHT_GRAY, width: width, alignSelf: "center", marginTop:10, ...style}}/>
     )
 }
